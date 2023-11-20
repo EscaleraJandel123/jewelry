a274d70c6da6168982c9ac51ecf7e0e1d3a42233
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-11-20 09:10:28 +0800

    Update cart.php

--- app/views/cart.php
+++ app/views/cart.php
@@ -194,7 +194,7 @@
                     <div class="card-body">
                         <div class="d-flex justify-content-between mb-3 pt-1">
                             <h6 class="font-weight-medium">Subtotal</h6>
-                            <h6 class="font-weight-medium">$150</h6>
+                            <h6 class="font-weight-medium">$<?= $c['prize'] * $c['quantity'] ?></h6>
                         </div>
                         <div class="d-flex justify-content-between">
                             <h6 class="font-weight-medium">Shipping</h6>
