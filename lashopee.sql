81a10323939828ecaaa562e5765602a21d59f05b
EscaleraJandel123 <134498020+EscaleraJandel123@users.noreply.github.com>
2023-11-30 10:30:58 +0800

    Update lashopee.sql

--- lashopee.sql
+++ lashopee.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 26, 2023 at 02:20 PM
+-- Generation Time: Nov 30, 2023 at 02:10 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -55,7 +55,11 @@ CREATE TABLE `cat` (
 INSERT INTO `cat` (`id`, `categories`) VALUES
 (11, 'Necklace'),
 (12, 'Diamond'),
-(13, 'Pearl');
+(15, 'Earrings'),
+(16, 'Ring'),
+(17, 'Tiara'),
+(18, 'Watch'),
+(19, 'Bracelet');
 
 -- --------------------------------------------------------
 
@@ -79,7 +83,14 @@ CREATE TABLE `prod` (
 --
 
 INSERT INTO `prod` (`id`, `name`, `image`, `description`, `date`, `category`, `quantity`, `prize`) VALUES
-(58, 'testing', 'img_656350ee960eb_4.jpg', '123', '2023-11-26 14:06:38', 'Diamond', 1, 1200);
+(62, 'Celestial Cascade Earrings', 'img_6567e794252fb_modern-brilliance-asymmetrical-sapphire-wedding-ring-platinum_941561-1995.jpg', ' Earrings', '2023-11-27 03:13:27', 'Necklace', 100, 1200),
+(63, 'Whispering Willow Pendant', 'img_656409b878ac8_pendamnt.jpg', 'A nature-inspired pendant shaped like a graceful willow tree, with intricate branches and leaves that evoke a sense of serenity.', '2023-11-27 03:15:04', 'Necklace', 100, 30),
+(65, 'Ethereal Eclipse Tiara', 'img_65640a760e8ea_tiara.avif', 'A regal tiara inspired by the celestial beauty of a solar eclipse, with delicate details that exude an ethereal and enchanting aura.', '2023-11-27 03:18:14', 'Necklace', 100, 1500),
+(66, 'Adorned Amethyst Pocket Watch', 'img_65640b0178bec_watch.jpeg', 'A vintage-style pocket watch adorned with a deep purple amethyst, adding a touch of sophistication to both casual and formal attire', '2023-11-27 03:20:33', 'Watch', 100, 1200),
+(67, 'Mystic Moonstone Ring', 'img_65640b99147ef_ring2.webp', 'A captivating ring featuring a shimmering moonstone, known for its ethereal glow, surrounded by intricate silver or gold detailing', '2023-11-27 03:22:28', 'Ring', 100, 1600),
+(68, 'Whirlwind Opal Cluster Ring', 'img_65640bd511805_ring 3.webp', 'A statement ring with a cluster of opals arranged in a swirling design, reminiscent of a mesmerizing whirlwind', '2023-11-27 03:24:05', 'Ring', 100, 1400),
+(69, 'Eternal Love Knot Ring', 'img_65640c0b51912_ring3.png', 'A symbolic ring with an eternal love knot design, representing the unbreakable bond and commitment between two individuals', '2023-11-27 03:24:59', 'Ring', 100, 1500),
+(70, 'Lunar Lapis Lazuli Bracelet', 'img_65640cac0c81f_brace.jpg', 'A bracelet adorned with lapis lazuli beads, each representing a moonlit night sky, bringing a sense of calm and celestial beauty', '2023-11-27 03:27:40', 'Bracelet', 100, 1600);
 
 -- --------------------------------------------------------
 
@@ -98,13 +109,6 @@ CREATE TABLE `products` (
   `category` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `name`, `description`, `price`, `weight`, `image`, `qty`, `category`) VALUES
-(1, 'dfsf', 'asdfasdfasfas', 2, '21', '', '21', 'qwe');
-
 -- --------------------------------------------------------
 
 --
@@ -114,14 +118,11 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `weight`, `image`,
 CREATE TABLE `purchase` (
   `id` int NOT NULL,
   `user_id` int NOT NULL,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
+  `fullname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `email` varchar(50) NOT NULL,
   `number` varchar(50) NOT NULL,
-  `street` varchar(50) NOT NULL,
-  `barangay` varchar(100) NOT NULL,
-  `city` varchar(50) NOT NULL,
-  `zip` varchar(50) NOT NULL,
+  `compAdd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `payment` varchar(255) NOT NULL,
   `order_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -129,43 +130,14 @@ CREATE TABLE `purchase` (
 -- Dumping data for table `purchase`
 --
 
-INSERT INTO `purchase` (`id`, `user_id`, `firstName`, `lastName`, `email`, `number`, `street`, `barangay`, `city`, `zip`, `order_at`) VALUES
-(201, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:00:36'),
-(202, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:02:26'),
-(203, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:11:30'),
-(204, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:12:38'),
-(205, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:14:20'),
-(206, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:14:42'),
-(207, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:19:54'),
-(208, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:20:07'),
-(209, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:24:05'),
-(210, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:24:28'),
-(211, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:24:44'),
-(212, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:25:21'),
-(213, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:26:47'),
-(214, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:27:45'),
-(215, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:28:38'),
-(216, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:29:23'),
-(217, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:47:07'),
-(218, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:49:41'),
-(219, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:50:40'),
-(220, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:54:40'),
-(221, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:56:01'),
-(222, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:08'),
-(223, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:10'),
-(224, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:11'),
-(225, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:11'),
-(226, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:12'),
-(227, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:12'),
-(228, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:13'),
-(229, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:13'),
-(230, 23, '', '', '', '', '', '', '', '', '2023-11-26 13:57:39'),
-(231, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:00:37'),
-(232, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:02:28'),
-(233, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:02:40'),
-(234, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:05:53'),
-(235, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:06:55'),
-(236, 23, '', '', '', '', '', '', '', '', '2023-11-26 14:15:45');
+INSERT INTO `purchase` (`id`, `user_id`, `fullname`, `email`, `number`, `compAdd`, `payment`, `order_at`) VALUES
+(238, 23, 'Jandel', 'jandeleido@gmail.com', '09366581432', 'St. Pagkakaisa', '', '2023-11-27 03:30:43'),
+(239, 23, 'Jandel', 'jandeleido@gmail.com', '123', 'Str132', '', '2023-11-27 03:33:18'),
+(240, 23, 'Jandel L. Escalera', 'jandeleido@gmail.com', '09366581432', 'Sitio Pagkakaisa, Lumangbayan, Calapan City', '', '2023-11-28 09:50:52'),
+(241, 23, 'Jandel L. Escalera', 'jandeleido@gmail.com', '09366581432', 'Sitio Pagkakaisa, Lumangbayan, Calapan City', '', '2023-11-28 10:08:31'),
+(242, 24, 'Alejandro Gino', 'alejandrogino950@gmail.com', '09366581432', 'Lumangbayan Calapan City', '', '2023-11-30 01:07:20'),
+(243, 23, 'Jandel L. Escalera', 'jandeleido@gmail.com', '09366581432', 'Sitio Pagkakaisa, Lumangbayan, Calapan City', '', '2023-11-30 01:26:39'),
+(244, 23, 'Jandel L. Escalera', 'jandeleido@gmail.com', '09366581432', 'Sitio Pagkakaisa, Lumangbayan, Calapan City', '', '2023-11-30 01:27:02');
 
 -- --------------------------------------------------------
 
@@ -184,16 +156,18 @@ CREATE TABLE `purchase_items` (
   `prize` decimal(10,2) DEFAULT NULL,
   `total_price` decimal(10,2) DEFAULT NULL,
   `order_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `prod_id` int DEFAULT NULL
+  `prod_id` int DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `purchase_items`
 --
 
-INSERT INTO `purchase_items` (`id`, `purchase_id`, `Item_image`, `Item_name`, `CustomerId`, `Customer`, `quantity`, `prize`, `total_price`, `order_at`, `prod_id`) VALUES
-(200, 236, 'img_656350ee960eb_4.jpg', 'testing', 23, ' ', 2, '1200.00', '2400.00', '2023-11-26 14:15:45', 58),
-(201, 236, 'img_656350ee960eb_4.jpg', 'testing', 23, ' ', 2, '1200.00', '2400.00', '2023-11-26 14:15:45', 58);
+INSERT INTO `purchase_items` (`id`, `purchase_id`, `Item_image`, `Item_name`, `CustomerId`, `Customer`, `quantity`, `prize`, `total_price`, `order_at`, `prod_id`, `status`) VALUES
+(208, 240, 'img_65640c0b51912_ring3.png', 'Eternal Love Knot Ring', 23, 'Jandel L. Escalera', 1, '1500.00', '1500.00', '2023-11-28 09:50:52', 69, 'Pending'),
+(209, 241, 'img_656409b878ac8_pendamnt.jpg', 'Whispering Willow Pendant', 23, 'Jandel L. Escalera', 10, '30.00', '300.00', '2023-11-28 10:08:31', 63, 'Pending'),
+(210, 242, 'img_65640b0178bec_watch.jpeg', 'Adorned Amethyst Pocket Watch', 24, 'Alejandro Gino', 3, '1200.00', '3600.00', '2023-11-30 01:07:20', 66, 'Pending');
 
 -- --------------------------------------------------------
 
@@ -205,19 +179,22 @@ CREATE TABLE `users` (
   `id` int NOT NULL,
   `email` text NOT NULL,
   `password` text NOT NULL,
-  `role` varchar(50) NOT NULL DEFAULT 'user'
+  `role` varchar(50) NOT NULL DEFAULT 'user',
+  `fullname` varchar(255) NOT NULL,
+  `compAdd` varchar(255) NOT NULL,
+  `number` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `role`) VALUES
-(23, 'jandeleido@gmail.com', '$2y$10$2AfLHVrLaP9FRXzs1qsHmunOHQ7UJh5wwauBlwTGG0PPBBlMUDhay', 'user'),
-(24, 'alejandrogino950@gmail.com', '$2y$10$adUGqOXybvPjMFQmHufIPO0uh9BIQi4nDwFsqAwBPD3oJwkiIzNjm', 'user'),
-(25, 'admin@gmail.com', '$2y$10$HOWOnXRztFtEB12saZdGuOrpaV5DgyF.bie8iQtxn83B.LrJ7XM76', 'admin'),
-(26, 'jan@gmail.com', '$2y$10$xP.vyVcbdr/HRl/ILET/eeRhaVhgN.FNZigjjBhMSKX.qFBOwygQ.', 'user'),
-(28, 'jan@gmail.com', '$2y$10$BXj/iD.5DPLE6FOBQo3FF.wE7p.rr7QdELLV8kg/7KERPu6/MrtcS', 'user');
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `fullname`, `compAdd`, `number`) VALUES
+(23, 'jandeleido@gmail.com', '$2y$10$2AfLHVrLaP9FRXzs1qsHmunOHQ7UJh5wwauBlwTGG0PPBBlMUDhay', 'user', 'Jandel L. Escalera', 'Sitio Pagkakaisa, Lumangbayan, Calapan City', '09366581432'),
+(24, 'alejandrogino950@gmail.com', '$2y$10$adUGqOXybvPjMFQmHufIPO0uh9BIQi4nDwFsqAwBPD3oJwkiIzNjm', 'user', 'Alejandro Gino', 'Lumangbayan Calapan City', '09366581432'),
+(25, 'admin@gmail.com', '$2y$10$HOWOnXRztFtEB12saZdGuOrpaV5DgyF.bie8iQtxn83B.LrJ7XM76', 'admin', '', '', ''),
+(26, 'jan@gmail.com', '$2y$10$xP.vyVcbdr/HRl/ILET/eeRhaVhgN.FNZigjjBhMSKX.qFBOwygQ.', 'user', '', '', ''),
+(28, 'jan@gmail.com', '$2y$10$BXj/iD.5DPLE6FOBQo3FF.wE7p.rr7QdELLV8kg/7KERPu6/MrtcS', 'user', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -274,19 +251,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- AUTO_INCREMENT for table `cat`
 --
 ALTER TABLE `cat`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `prod`
 --
 ALTER TABLE `prod`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -298,13 +275,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `purchase`
 --
 ALTER TABLE `purchase`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=237;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=245;
 
 --
 -- AUTO_INCREMENT for table `purchase_items`
 --
 ALTER TABLE `purchase_items`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=214;
 
 --
 -- AUTO_INCREMENT for table `users`
